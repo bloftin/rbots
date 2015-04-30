153338499ee52c446c1560481bb5d3bda6ca0264
Ben Loftin <ben.loftin@gmail.com>
2015-04-30 08:54:24 -0700

    Archive Droid 4
    Archive Droid 4

---
+++ Archive/Arduino/Droid/HelloAndroid/src/com/example/helloandroid/HelloAndroid.java
@@ -0,0 +1,16 @@
+package com.example.helloandroid;
+
+import android.app.Activity;
+import android.os.Bundle;
+ 
+
+public class HelloAndroid extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+        
+
+    }
+}
