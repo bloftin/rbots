6df23a9b3405a89723a798436757f1a900f57917
artanz <davidgfong@gmail.com>
2016-08-09 21:14:35 -0700

    Add files via upload

---
+++ SumoExample/sumobot_class_example/motor.h
@@ -0,0 +1,66 @@
+#ifndef Motor_h
+#define Motor_h
+
+//motor control pins
+int PWM_L = 9;   //PWM control for motor outputs 1 and 2 is on digital pin 10
+int PWM_R = 10;   //PWM control for motor outputs 3 and 4 is on digital pin 11
+int DIR_L = 5;   //direction control for motor outputs 1 and 2 is on digital pin 12
+int DIR_R = 6;  //direction control for motor outputs 3 and 4 is on digital pin 13
+
+//Full Motor Speed
+#define MAXSPEED 200
+//Half Motor Speed
+#define HALFSPEED 100
+//Quarter Motor Speed
+#define QUARTERSPEED 50
+
+void motor_init()
+{
+  pinMode(PWM_L, OUTPUT);  //Set control pins to be outputs
+  pinMode(PWM_R, OUTPUT);
+  pinMode(DIR_L, OUTPUT);
+  pinMode(DIR_R, OUTPUT);
+  analogWrite(PWM_L,0);
+  analogWrite(PWM_R,0);
+}
+
+void idle_motor()
+{
+  analogWrite(PWM_L,0);
+  analogWrite(PWM_R,0);
+}
+
+void forward(int left_speed, int right_speed)
+{
+  digitalWrite(DIR_L, HIGH);
+  digitalWrite(DIR_R, HIGH);
+  analogWrite(PWM_L, left_speed);
+  analogWrite(PWM_R, right_speed);
+  
+}
+
+void backward(int left_speed, int right_speed)
+{
+  digitalWrite(DIR_L, LOW);
+  digitalWrite(DIR_R, LOW);
+  analogWrite(PWM_L, left_speed);
+  analogWrite(PWM_R, right_speed);
+}
+
+void rotateleft(int rotate_speed)
+{
+  digitalWrite(DIR_L, LOW);
+  digitalWrite(DIR_R, HIGH);
+  analogWrite(PWM_L, rotate_speed);
+  analogWrite(PWM_R, rotate_speed);
+}
+
+void rotateright(int rotate_speed)
+{
+  digitalWrite(DIR_L, HIGH);
+  digitalWrite(DIR_R, LOW);
+  analogWrite(PWM_L, rotate_speed);
+  analogWrite(PWM_R, rotate_speed);
+}
+
+#endif // Motor_h
---
+++ SumoExample/sumobot_class_example/sumobot_class_example.ino
@@ -0,0 +1,192 @@
+/* Mini Sumo Code */
+// Written by D. Fong
+//
+// Compatible with Arduino IDE 1.6.10
+// Compatible with Arduino Trinket Pro 5V
+// 
+// Releases:
+// v1.00 Initial Release to R-Team Robotics Community
+
+#include "sumosense.h"
+#include "motor.h"
+#include "edgesensor.h"
+
+#include <IRremote.h>
+
+// ir remote pin
+int irremote_pin = A0;
+IRrecv irrecv(irremote_pin);
+decode_results signals;
+
+// time variable
+long cycletime = 0;
+
+enum sumostate {
+  IDLESTATE,
+  STARTFIVESEC,
+  SEARCH,
+  TRACKTEST,
+  SENSORON,
+  EDGEAVOIDLEFT,        // state where sumo detects edge of ring and will try to turn around
+  EDGEAVOIDRIGHT };    // state where sumo detects edge of ring and will try to turn around
+ 
+typedef enum sumostate sumo;
+long searchtime = 0;     // time when sumo enters find state
+long hunttime = 0;     // time when sumo enters hunt state
+long edgetime = 0;
+
+//sumo states
+sumo current;
+sumo nextstate;
+
+void setup() {
+  Serial.begin(9600);  // Initializes serial port
+  // Waits for serial port to connect. Needed for Leonardo only
+  while ( !Serial ) ;
+
+  motor_init();
+  edge_init();
+  ir_prox_init();
+
+  //enabe input from IR remote receiver
+  irrecv.enableIRIn();
+
+  // start motors in idle/brake
+  idle_motor();
+}
+
+void loop() {
+    cycletime = millis();
+    bool edgeleft = false;
+    bool edgeright = false;
+
+    // Check if at edge
+    edgeleft = left_edge();
+    edgeright = right_edge();
+    if (edgeleft == true && current == SEARCH) current = EDGEAVOIDLEFT;
+    if (edgeright == true && current == SEARCH) current = EDGEAVOIDRIGHT;
+    
+    switch (current) {
+        case IDLESTATE:
+          //Serial.println("IDLE STATE"); 
+          idle_motor();
+          break;
+        case STARTFIVESEC:
+          //Serial.println("START 5 SEC STATE"); 
+          delay(5000);
+          nextstate = SEARCH;
+          break;
+        case SEARCH:
+          if (ir_detect_front == true && ir_detect_left == false && ir_detect_right == false) {
+            forward(MAXSPEED, MAXSPEED);
+          }
+          else if (ir_detect_front == true && ir_detect_left == true && ir_detect_right == true) {
+            forward(MAXSPEED, MAXSPEED);
+          }
+          else if (ir_detect_front == false && ir_detect_left == true && ir_detect_right == false) {
+            rotateleft(MAXSPEED);
+          }
+          else if (ir_detect_front == true && ir_detect_left == true && ir_detect_right == false) {
+            forward(HALFSPEED, MAXSPEED);
+          }
+          else if (ir_detect_front == false && ir_detect_left == false && ir_detect_right == true) {
+            rotateright(MAXSPEED);
+          }
+          else if (ir_detect_front == true && ir_detect_left == false && ir_detect_right == true) {
+            forward(MAXSPEED, HALFSPEED);
+          }
+          else {
+            forward(HALFSPEED, HALFSPEED);
+          }
+          nextstate = SEARCH;  
+          break;
+        case TRACKTEST:
+          nextstate = TRACKTEST;
+          if (ir_detect_front == true && ir_detect_left == false && ir_detect_right == false) {
+            idle_motor();
+          }
+          else if (ir_detect_front == true && ir_detect_left == true && ir_detect_right == true) {
+            idle_motor();
+          }
+          else if (ir_detect_front == false && ir_detect_left == true && ir_detect_right == false) {
+            rotateleft(HALFSPEED);
+          }
+          else if (ir_detect_front == true && ir_detect_left == true && ir_detect_right == false) {
+            rotateleft(HALFSPEED);
+          }
+          else if (ir_detect_front == false && ir_detect_left == false && ir_detect_right == true) {
+            rotateright(HALFSPEED);
+          }
+          else if (ir_detect_front == true && ir_detect_left == false && ir_detect_right == true) {
+            rotateright(HALFSPEED);
+          }
+          else {
+            idle_motor();
+          }
+          break;
+        case SENSORON:
+          Serial.println(detect_ir, BIN);
+          if (ir_detect_front == true && ir_detect_left == false && ir_detect_right == false) {
+            Serial.println("DETECT FRONT");
+          }
+          else if (ir_detect_front == true && ir_detect_left == true && ir_detect_right == true) {
+            Serial.println("DETECT FRONT");
+          }
+          else if (ir_detect_front == false && ir_detect_left == true && ir_detect_right == false) {
+            Serial.println("DETECT LEFT");
+          }
+          else if (ir_detect_front == true && ir_detect_left == true && ir_detect_right == false) {
+            Serial.println("DETECT LEFT");
+          }
+          else if (ir_detect_front == false && ir_detect_left == false && ir_detect_right == true) {
+            Serial.println("DETECT RIGHT");
+          }
+          else if (ir_detect_front == true && ir_detect_left == false && ir_detect_right == true) {
+            Serial.println("DETECT RIGHT");
+          }
+          else {
+            Serial.println("DETECT NONE");
+          }
+          nextstate = SENSORON;
+          break;
+        case EDGEAVOIDLEFT:
+          backward(MAXSPEED, MAXSPEED);
+          delay(200);
+          rotateleft(MAXSPEED);
+          delay(300);
+          nextstate = SEARCH;
+          break;
+        case EDGEAVOIDRIGHT:
+          backward(MAXSPEED, MAXSPEED);
+          delay(200);
+          rotateright(MAXSPEED);
+          delay(300);
+          nextstate = SEARCH;
+          break;
+        default:
+          nextstate = IDLESTATE;
+          break;
+    }
+    
+    if (irrecv.decode(&signals)) 
+    {
+      // Serial.println(signals.value, HEX);
+      if (signals.value == 0xFFC23D) nextstate = STARTFIVESEC;
+      if (signals.value == 0xFF6897) nextstate = IDLESTATE;
+      if (signals.value == 0xFF7A85) nextstate = TRACKTEST;
+      if (signals.value == 0xFF10EF) nextstate = SENSORON;
+      irrecv.resume(); // get the next signal
+      }
+
+    
+    if (current != IDLESTATE) {
+      ir_prox_update();
+    }
+    
+    //Serial.print("Main Loop Time : ");
+    //Serial.print(millis() - cycletime);
+    //Serial.println("");
+    
+    current = nextstate;
+    
+}
